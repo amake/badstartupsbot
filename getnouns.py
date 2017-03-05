353e3bc2b641b93f67d39bc879f38b59e7cc3889
Aaron Madlon-Kay <aaron@madlon-kay.com>
2017-03-05 20:26:38 +0900

    Try not to pluralize proper nouns

--- getnouns.py
+++ getnouns.py
@@ -1,14 +1,18 @@
 from nltk.corpus import wordnet
+from nltk.tag import pos_tag
 from pattern.en import pluralize
 
 with open('nouns.txt', 'w') as ofile:
     words = set(lemma.name().replace('_', ' ')
                 for synset in wordnet.all_synsets(wordnet.NOUN)
                 for lemma in synset.lemmas())
-    for word in sorted(words):
-        plural = pluralize(word)
-        if plural.endswith('ss'):
+    for word, tag in pos_tag(sorted(words)):
+        if tag == 'NNP' and word.istitle():
             ofile.write(word)
         else:
-            ofile.write(plural)
+            plural = pluralize(word)
+            if plural.endswith('ss'):
+                ofile.write(word)
+            else:
+                ofile.write(plural)
         ofile.write('\n')
